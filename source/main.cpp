11119f6ec5fd49392db4dbec74aa666ef8c1b0b7
Battisti-Daniel <danielbattisti@outlook.com.br>
2026-02-25 21:23:59 -0300

    First Commit

---
+++ source/main.cpp
@@ -0,0 +1,28 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "GameDevelopmentProject", nullptr, nullptr);
+
+    if (window == nullptr) {
+        std::cout << "Error creating window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSetWindowPos(window, 2000, 150);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+
+}
